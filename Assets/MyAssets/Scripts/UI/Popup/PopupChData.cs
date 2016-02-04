a24fadcf53822387f644b0569f546f01d5122bdf
장진우 <zzangjjw4406@gmail.com>
2016-02-05 00:23:42 +0900

    팝업창 이펙트 스크립트 추가.
    1. 팝업창 on/off 시 이펙트 관련 스크립트 추가.

--- Assets/MyAssets/Scripts/UI/Popup/PopupChData.cs
+++ Assets/MyAssets/Scripts/UI/Popup/PopupChData.cs
@@ -12,14 +12,47 @@ public class PopupChData : MonoBehaviour
     private UILabel chType;
     [SerializeField]
     private UILabel chDetailScript;
+    [SerializeField]
+    private GameObject popupObj;
 
     void Start()
     {
         SetData();
+        ScaleUpEffect();
+    }
+
+    private void ScaleUpEffect()
+    {
+        popupObj.transform.localScale = new Vector3(0, 0, 0);
+        Vector3 scaleUp = new Vector3(1, 1, 1);
+        iTween.ScaleTo(popupObj, iTween.Hash("scale", scaleUp,
+            "name", "scaleUp",
+            "time", 1.0f,
+            "speed", 10.0f,
+            "easetype", iTween.EaseType.linear,
+            "looptype", iTween.LoopType.none));
+    }
+    private void ScaleDownEffect()
+    {
+        popupObj.transform.localScale = new Vector3(1, 1, 1);
+        Vector3 scaleUp = new Vector3(0, 0, 0);
+        iTween.ScaleTo(popupObj, iTween.Hash("scale", scaleUp,
+            "name", "scaleUp",
+            "time", 1.0f,
+            "speed", 10.0f,
+            "easetype", iTween.EaseType.linear,
+            "looptype", iTween.LoopType.none));
     }
 
     public void ClickExit()
     {
+        StartCoroutine(PopupExitProcess());        
+    }
+
+    private IEnumerator PopupExitProcess()
+    {
+        ScaleDownEffect();
+        yield return new WaitForSeconds(1.0f);
         SceneManager.UnloadScene("popup_chInfo");
     }
 
