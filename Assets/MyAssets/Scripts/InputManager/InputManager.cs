b617e70dceabee5434ef57ee68f90e464fe938f2
장진우 <zzangjjw4406@gmail.com>
2016-02-13 01:27:41 +0900

    InputManager 클래스 수정 및 테스트
    1. 입력매니저 클래스 수정 및 테스트
    2. 블록 삭제 & 생성에 대한 case만 구현.
    3. 이후 공격에 대한 부분 구현 예정
    4. 나중에 사용자 정보를 연동시켜 인벤토리 아이템 및 공격에 대한 부분을 세부적으로 구현할 생각.

--- Assets/MyAssets/Scripts/InputManager/InputManager.cs
+++ Assets/MyAssets/Scripts/InputManager/InputManager.cs
@@ -8,6 +8,16 @@ public class InputManager : MonoBehaviour {
 
     private Ray screenToWorldRay;
     private RaycastHit rayHit;
+    private enum INPUT_STATE
+    {
+        NONE = 0,
+        CREATE = 1,
+        DELETE = 2,
+        ATTACK = 3
+    }
+    private INPUT_STATE inputState = INPUT_STATE.NONE;
+
+    private bool isInput = false;
     
     void Start()
     {
@@ -16,33 +26,59 @@ public class InputManager : MonoBehaviour {
 
 	void Update ()
     {
-        if (Input.GetMouseButtonDown(0)) // left
+        ChkInputState();
+        if (ChkRayHit()) InputProcess();
+    }
+
+    private void ChkInputState()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            screenToWorldRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(screenToWorldRay, out rayHit))
-            {
-                InitModifyProcess();
+            isInput = true;
+            inputState = INPUT_STATE.CREATE;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            isInput = true;
+            inputState = INPUT_STATE.DELETE;
+        }
+        else
+        {
+            isInput = false;
+            inputState = INPUT_STATE.NONE;
+        }
+    }
+
+    private bool ChkRayHit()
+    {
+        if (isInput == false) return false;
+
+        screenToWorldRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        return Physics.Raycast(screenToWorldRay, out rayHit);
+    }
+
+    private void InputProcess()
+    {
+        InitModifyProcess();
+        switch (inputState)
+        {
+            case INPUT_STATE.CREATE:
                 Debug.DrawLine(screenToWorldRay.origin,
                     screenToWorldRay.origin + (screenToWorldRay.direction * rayHit.distance),
                     Color.green, 2);
-                modifyTerrian.AddBlockCursor(rayHit, 1);
-            }
-        }
-        else if (Input.GetMouseButtonDown(1)) // right
-        {
-            screenToWorldRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(screenToWorldRay, out rayHit))
-            {
-                InitModifyProcess();
+                modifyTerrian.AddBlockCursor(rayHit, 4);
+                break;
+            case INPUT_STATE.DELETE:
                 Debug.DrawLine(screenToWorldRay.origin,
                     screenToWorldRay.origin + (screenToWorldRay.direction * rayHit.distance),
                     Color.green, 2);
                 modifyTerrian.ReplaceBlockCursor(rayHit, 0);
-            }
+                break;
+            default:
+                break;
         }
     }
 
-   
     private void InitModifyProcess()
     {
         World world = rayHit.transform.GetComponent<Chunk>().world;
