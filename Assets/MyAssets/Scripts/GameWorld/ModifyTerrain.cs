8da88389d177966d30fb39038129ab64f219f3ba
장진우 <kojeomstudio@gmail.com>
2016-11-03 15:00:06 +0900

    # 지형 삭제/추가 하는 코드 수정.
    1. ModifyTerrain 스크립트 코드 일부 수정
    -> Blcok 클래스가 가지고 있던 AABB를 삭제.
    -> 기타 코드 정리.

--- Assets/MyAssets/Scripts/GameWorld/ModifyTerrain.cs
+++ Assets/MyAssets/Scripts/GameWorld/ModifyTerrain.cs
@@ -18,11 +18,11 @@ public class ModifyTerrain : MonoBehaviour
     [SerializeField]
     private GameManager gameMgr;
 
-    private World _world;
-    public World world
-    {
-        set { _world = value; }
-    }
+    private World world;
+    //public World world
+    //{
+    //    set { _world = value; }
+    //}
     
     private int chunkSize = 0;
 
@@ -43,35 +43,36 @@ public class ModifyTerrain : MonoBehaviour
 
     private void DeleteBlockAt(Ray ray, byte blockType)
     {
-        _world = gameMgr.worldList[0];
+        world = gameMgr.worldList[0];
         RayCastingProcess(ray, blockType, false);
     }
     private void AddBlockAt(Ray ray, byte blockType)
     {
-        _world = gameMgr.worldList[0];
+        world = gameMgr.worldList[0];
         RayCastingProcess(ray, blockType, true);
     }
 
     private void RayCastingProcess(Ray ray, byte blockType, bool isCreate)
     {
-        CollideInfo collideInfo = _world.customOctree.Collide(ray);
+        CollideInfo collideInfo = world.customOctree.Collide(ray);
         if (collideInfo.isCollide)
         {
             int blockX, blockY, blockZ;
             blockX = (int)(collideInfo.hitBlockCenter.x);
             blockY = (int)(collideInfo.hitBlockCenter.y);
             blockZ = (int)(collideInfo.hitBlockCenter.z);
-            //gameMgr.GetYuKoNPC().ActivePathFindNPC(blockX, blockZ);
+            // 광선과 충돌하는 블록으로 NPC가 이동한다. ( 길찾기 알고리즘 테스트용 코드. )--------
+            gameMgr.GetYuKoNPC().ActivePathFindNPC(blockX, blockZ);
+            //-------------------------------------------------------------------------------
             if (isCreate)
             {
-                _world.customOctree.Add(collideInfo.hitBlockCenter + new Vector3(0, 1.0f, 0));
+                world.customOctree.Add(collideInfo.hitBlockCenter + new Vector3(0, 1.0f, 0));
                 SetBlockForAdd(blockX, blockY + 1, blockZ, blockType);
             }
             else
             {
-                _world.customOctree.Delete(collideInfo.hitBlockCenter);
+                world.customOctree.Delete(collideInfo.hitBlockCenter);
                 SetBlockForDelete(blockX, blockY, blockZ, blockType);
-                gameMgr.worldList[0].worldBlockData[blockX, blockY, blockZ].aabb.isEnable = false;
             }
         }
     }
@@ -84,9 +85,8 @@ public class ModifyTerrain : MonoBehaviour
            (z < GameWorldConfig.worldZ) &&
            (x >= 0) && (y >= 0) && (z >= 0)) 
         {
-            _world.worldBlockData[x, y, z].type = block;
-            _world.worldBlockData[x, y, z].isRendered = true;
-            _world.worldBlockData[x, y, z].aabb.isEnable = true;
+            world.worldBlockData[x, y, z].type = block;
+            world.worldBlockData[x, y, z].isRendered = true;
             UpdateChunkAt(x, y, z, block);
         }
         else
@@ -109,37 +109,45 @@ public class ModifyTerrain : MonoBehaviour
             IDbCommand dbcmd;
             using (dbconn = (IDbConnection)new SqliteConnection(conn))
             {
-                dbconn.Open(); //Open connection to the database.
-                string sqlQuery = "INSERT INTO USER_ITEM (name, type, amount) VALUES("
-                                   + "'"+ itemName +"'" + "," + "'" +type + "'" + "," + "1)";
-                dbcmd.CommandText = sqlQuery;
-                dbcmd.ExecuteNonQuery();
+                using (dbcmd = dbconn.CreateCommand())
+                {
+                    string itemName;
+                    itemName = lootingSystem.GetTypeToItemName(blockType.ToString());
+                    string type;
+                    ItemInfo itemInfo = itemDataFile.GetItemData(itemName);
+                    type = itemInfo.type;
+                    try
+                    {
+                        dbconn.Open(); //Open connection to the database.
+                        string sqlQuery = "INSERT INTO USER_ITEM (name, type, amount) VALUES("
+                                           + "'" + itemName + "'" + "," + "'" + type + "'" + "," + "1)";
+                        dbcmd.CommandText = sqlQuery;
+                        dbcmd.ExecuteNonQuery();
 
-                dbcmd.Dispose();
-                dbcmd = null;
+                        dbconn.Close();
+                    }
+                    catch (SqliteException e) // 인벤토리에 중복된 아이템이 있다면, 수량증가를 해야한다.
+                    {
+                        if (SQLiteErrorCode.Constraint == e.ErrorCode)
+                        {
+                            string sqlQuery = "SELECT amount FROM USER_ITEM WHERE name = "
+                                        + "'" + itemName + "'";
+                            dbcmd.CommandText = sqlQuery;
+                            IDataReader reader = dbcmd.ExecuteReader();
+                            reader.Read();
+                            int itemAmount = reader.GetInt32(0);
+                            itemAmount++;
+                            reader.Close();
 
-                dbconn.Close();
-                dbconn = null;
-            }
-            catch // 인벤토리에 중복된 아이템이 있다면, 수량증가를 해야한다.
-            {
-                string sqlQuery = "SELECT amount FROM USER_ITEM WHERE name = "
-                                  + "'" + itemName +"'";
-                dbcmd.CommandText = sqlQuery;
-                IDataReader reader = dbcmd.ExecuteReader();
-                reader.Read();
-                int itemAmount = reader.GetInt32(0);
-                itemAmount++;
-                reader.Close();
+                            sqlQuery = "UPDATE USER_ITEM SET amount = " + "'" + itemAmount + "'" +
+                                        " WHERE name = " + "'" + itemName + "'";
+                            dbcmd.CommandText = sqlQuery;
+                            dbcmd.ExecuteNonQuery();
 
-                sqlQuery = "UPDATE USER_ITEM SET amount = " + "'" + itemAmount + "'" +
-                            " WHERE name = " + "'" + itemName + "'" ;
-                dbcmd.CommandText = sqlQuery;
-                dbcmd.ExecuteNonQuery();
-
-                dbcmd.Dispose();
-                dbcmd = null;
-
+                            dbconn.Close();
+                        }
+                    }
+                }
                 dbconn.Close();
             }
         };
@@ -149,10 +157,9 @@ public class ModifyTerrain : MonoBehaviour
            (z < GameWorldConfig.worldZ) &&
            (x >= 0) && (y >= 0) && (z >= 0))
         {
-            UpdateUserItem(_world.worldBlockData[x, y, z].type);
-            _world.worldBlockData[x, y, z].type = block;
-            _world.worldBlockData[x, y, z].isRendered = false;
-            _world.worldBlockData[x, y, z].aabb.isEnable = false;
+            UpdateUserItem(world.worldBlockData[x, y, z].type);
+            world.worldBlockData[x, y, z].type = block;
+            world.worldBlockData[x, y, z].isRendered = false;
             UpdateChunkAt(x, y, z, block);
         }
         else
@@ -172,36 +179,36 @@ public class ModifyTerrain : MonoBehaviour
         updateY = Mathf.FloorToInt(y / chunkSize);
         updateZ = Mathf.FloorToInt(z / chunkSize);
 
-        _world.chunkGroup[updateX, updateY, updateZ].update = true;
+        world.chunkGroup[updateX, updateY, updateZ].update = true;
 
         if (x - (chunkSize * updateX) == 0 && updateX != 0)
         {
-            _world.chunkGroup[updateX - 1, updateY, updateZ].update = true;
+            world.chunkGroup[updateX - 1, updateY, updateZ].update = true;
         }
 
-        if (x - (chunkSize * updateX) == GameWorldConfig.chunkSize && updateX != _world.chunkGroup.GetLength(0) - 1)
+        if (x - (chunkSize * updateX) == GameWorldConfig.chunkSize && updateX != world.chunkGroup.GetLength(0) - 1)
         {
-            _world.chunkGroup[updateX + 1, updateY, updateZ].update = true;
+            world.chunkGroup[updateX + 1, updateY, updateZ].update = true;
         }
 
         if (y - (chunkSize * updateY) == 0 && updateY != 0)
         {
-            _world.chunkGroup[updateX, updateY - 1, updateZ].update = true;
+            world.chunkGroup[updateX, updateY - 1, updateZ].update = true;
         }
 
-        if (y - (chunkSize * updateY) == GameWorldConfig.chunkSize && updateY != _world.chunkGroup.GetLength(1) - 1)
+        if (y - (chunkSize * updateY) == GameWorldConfig.chunkSize && updateY != world.chunkGroup.GetLength(1) - 1)
         {
-            _world.chunkGroup[updateX, updateY + 1, updateZ].update = true;
+            world.chunkGroup[updateX, updateY + 1, updateZ].update = true;
         }
 
         if (z - (chunkSize * updateZ) == 0 && updateZ != 0)
         {
-            _world.chunkGroup[updateX, updateY, updateZ - 1].update = true;
+            world.chunkGroup[updateX, updateY, updateZ - 1].update = true;
         }
 
-        if (z - (chunkSize * updateZ) == GameWorldConfig.chunkSize && updateZ != _world.chunkGroup.GetLength(2) - 1)
+        if (z - (chunkSize * updateZ) == GameWorldConfig.chunkSize && updateZ != world.chunkGroup.GetLength(2) - 1)
         {
-            _world.chunkGroup[updateX, updateY, updateZ + 1].update = true;
+            world.chunkGroup[updateX, updateY, updateZ + 1].update = true;
         }
 
     }
