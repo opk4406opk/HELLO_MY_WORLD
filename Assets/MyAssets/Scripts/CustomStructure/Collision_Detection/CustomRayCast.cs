c8e09f08022e188d90666f4cdc83675f04daba01
장진우 <kojeomstudio@gmail.com>
2017-03-11 20:08:32 +0900

    # RayCasting 코드 수정 및 텍스트매쉬 프로 에셋 추가 후 삭제.
    1. RayCasting 코드 수정
    -> AABB, OBB에 범용적으로 광선 충돌 판정에 쓸 수 있는 메소드 추가.
    2. 텍스트 매쉬 프로 에셋 다운 후 다시 삭제.

--- Assets/MyAssets/Scripts/CustomStructure/Collision_Detection/CustomRayCast.cs
+++ Assets/MyAssets/Scripts/CustomStructure/Collision_Detection/CustomRayCast.cs
@@ -2,9 +2,59 @@
 using System.Collections;
 
 // ref #1 : http://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-box-intersection
-
+// ref #2 : http://www.opengl-tutorial.org/miscellaneous/clicking-on-objects/picking-with-custom-ray-obb-function/
 public class CustomRayCast : MonoBehaviour
 {
+    /// <summary>
+    /// min, maxExtent를 이용한 광선과의 충돌 판정 메소드.
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <param name="obb"></param>
+    /// <returns></returns>
+    public static bool InterSectWithBOX(Ray ray, Vector3 minExtent, Vector3 maxExtent)
+    {
+        Vector3 dirfrac;
+        float t;
+        // r.direction is unit direction vector of ray
+        dirfrac.x = 1.0f / ray.direction.x;
+        dirfrac.y = 1.0f / ray.direction.y;
+        dirfrac.z = 1.0f / ray.direction.z;
+        // aabb.minExtent is the corner of AABB with minimal coordinates - left bottom, aabb.maxExtent is maximal corner
+        // r.origin is origin of ray
+        float t1 = (minExtent.x - ray.origin.x) * dirfrac.x;
+        float t2 = (maxExtent.x - ray.origin.x) * dirfrac.x;
+        float t3 = (minExtent.y - ray.origin.y) * dirfrac.y;
+        float t4 = (maxExtent.y - ray.origin.y) * dirfrac.y;
+        float t5 = (minExtent.z - ray.origin.z) * dirfrac.z;
+        float t6 = (maxExtent.z - ray.origin.z) * dirfrac.z;
+
+        float tmin = Mathf.Max(Mathf.Max(Mathf.Min(t1, t2), Mathf.Min(t3, t4)), Mathf.Min(t5, t6));
+        float tmax = Mathf.Min(Mathf.Min(Mathf.Max(t1, t2), Mathf.Max(t3, t4)), Mathf.Max(t5, t6));
+
+        // if tmax < 0, ray (line) is intersecting AABB, but whole AABB is behing us
+        if (tmax < 0)
+        {
+            t = tmax;
+            return false;
+        }
+
+        // if tmin > tmax, ray doesn't intersect AABB
+        if (tmin > tmax)
+        {
+            t = tmax;
+            return false;
+        }
+
+        t = tmin;
+        return true;
+    }
+
+    /// <summary>
+    /// AABB 경계상자와 광선과의 충돌 판정 메소드.
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <param name="aabb"></param>
+    /// <returns></returns>
     public static bool InterSectWithAABB(Ray ray, CustomAABB aabb)
     {
         float tmin = (aabb.minExtent.x - ray.origin.x) / ray.direction.normalized.x;
