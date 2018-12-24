daaf5e9045be35022fd97f40ac08301cc34fbd28
Kuma_jjw <kojeomstudio@gmail.com>
2018-12-24 22:37:27 +0900

    # 코드 정리.

---
+++ Assets/MyAssets/Scripts/Network/tcp/SocketNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 참고 문서 : https://docs.microsoft.com/ko-kr/dotnet/framework/network-programming/asynchronous-client-socket-example
+
+/// <summary>
+/// TCP 소켓 네트워킹 매니저.
+/// </summary>
+public class SocketNetworkManager : MonoBehaviour {
+
+    private static SocketNetworkManager _singleton = null;
+    public static SocketNetworkManager singleton
+    {
+        get
+        {
+            if (_singleton == null) KojeomLogger.DebugLog("SocketNetworkManager 초기화 되지 않았습니다", LOG_TYPE.ERROR);
+            return _singleton;
+        }
+    }
+    public void Init()
+    {
+        
+    }
+
+    public void ConnectToServer()
+    {
+        
+    }
+}
