08a172969f2ff72dc56a6e848139ab3ead780edc
Kuma_jjw <kojeomstudio@gmail.com>
2019-05-19 01:55:35 +0900

    # 실험적인 기능 테스트에 사용되는 Script/Scene 파일 추가.

---
+++ Assets/MyAssets/Scripts/Experiments/TEST_Experiment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_Experiment : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform GamePlayer;
+    public Transform[] Enemyies;
+    public Transform EnemyCenterTarget;
+
+    public GameObject MainCameraInstance;
+    //
+    public float CamFOV = 60.0f;
+    public Vector3 DeltaCamPosition;
+    public Vector3 DeltaCamRotation;
+
+    void Start()
+    {
+        InitMainCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotationMainCamera();
+    }
+
+    private void InitMainCamera()
+    {
+        MainCameraInstance.transform.position = GamePlayer.position + DeltaCamPosition;
+    }
+
+    private void RotationMainCamera()
+    {
+        Vector3 dir = EnemyCenterTarget.position - GamePlayer.position;
+        dir.Normalize();
+        //
+        MainCameraInstance.transform.rotation.SetLookRotation(dir + DeltaCamRotation);
+        MainCameraInstance.transform.SetPositionAndRotation(MainCameraInstance.transform.position, Quaternion.FromToRotation(GamePlayer.forward, dir) * Quaternion.Euler(DeltaCamRotation));
+        MainCameraInstance.GetComponent<Camera>().fieldOfView = CamFOV;
+    }
+}
